962597578cc444435a92b8803b79e49692adf934
Snomn <simon.alexander.pettersen@gmail.com>
2022-10-22 12:15:59 +0200

    Boatfly settings

--- src/main/java/net/snomn/snowhack/hack/misc/BoatFly.java
+++ src/main/java/net/snomn/snowhack/hack/misc/BoatFly.java
@@ -4,19 +4,25 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.Vec3d;
 import net.snomn.snowhack.hack.Hack;
+import net.snomn.snowhack.hack.settings.ModeSetting;
+import net.snomn.snowhack.hack.settings.NumberSetting;
 import org.lwjgl.glfw.GLFW;
 
 public class BoatFly extends Hack {
 
+    public NumberSetting speed = new NumberSetting("Speed", 0, 10, 3, 0.1);
+    public ModeSetting modes = new ModeSetting("Modes", "Bypass", "Bypass", "Free");
+
     public BoatFly() {
         super("BoatFly", "Makes boats fly ;)", Category.MISC);
         this.setKey(GLFW.GLFW_KEY_B);
+        addSettings(speed, modes);;
     }
 
     int toggle = 0;
-    int MAX_SPEED = 3;
+   // int MAX_SPEED = 3;
     double FALL_SPEED = -0.04;
-    double acceleration = 0.1;
+   double acceleration = 0.1;
 
     MinecraftClient mc = MinecraftClient.getInstance();
 
@@ -44,40 +50,42 @@ public class BoatFly extends Hack {
                 object = mc.player.getVehicle();
             }
 
-            Vec3d velocity = object.getVelocity();
-            Vec3d newVelocity = new Vec3d(velocity.x, -FALL_SPEED, velocity.z);
-            if(jumpPressed) {
+            if(mc.player.hasVehicle()) {
+                Vec3d velocity = object.getVelocity();
+                Vec3d newVelocity = new Vec3d(velocity.x, -FALL_SPEED, velocity.z);
+                if(jumpPressed) {
 
-                if(forwardPressed) {
-                    newVelocity = mc.player.getRotationVector().multiply(acceleration);
-                }
-                if(leftPressed && !mc.player.hasVehicle()) {
-                    newVelocity = mc.player.getRotationVector().multiply(acceleration).rotateY(3.14159227F/2);
-                    newVelocity = new Vec3d(newVelocity.x, 0, newVelocity.z);
-                }
-                if(rightPressed && !mc.player.hasVehicle()) {
-                    newVelocity = mc.player.getRotationVector().multiply(acceleration).rotateY(-3.14159227F/2);
-                    newVelocity = new Vec3d(newVelocity.x, 0, newVelocity.z);
-                }
-                if(backPressed) {
-                    newVelocity = mc.player.getRotationVector().negate().multiply(acceleration);
-                }
+                    if(forwardPressed) {
+                        newVelocity = mc.player.getRotationVector().multiply(acceleration);
+                    }
+                    if(leftPressed && !mc.player.hasVehicle()) {
+                        newVelocity = mc.player.getRotationVector().multiply(acceleration).rotateY(3.14159227F/2);
+                        newVelocity = new Vec3d(newVelocity.x, 0, newVelocity.z);
+                    }
+                    if(rightPressed && !mc.player.hasVehicle()) {
+                        newVelocity = mc.player.getRotationVector().multiply(acceleration).rotateY(-3.14159227F/2);
+                        newVelocity = new Vec3d(newVelocity.x, 0, newVelocity.z);
+                    }
+                    if(backPressed) {
+                        newVelocity = mc.player.getRotationVector().negate().multiply(acceleration);
+                    }
 
-                newVelocity = new Vec3d(newVelocity.x, (toggle == 0 && newVelocity.y > FALL_SPEED) ? FALL_SPEED : newVelocity.y, newVelocity.z);
-                object.setVelocity(newVelocity);
+                    newVelocity = new Vec3d(newVelocity.x, (modes.isMode("Bypass") && toggle == 0 && newVelocity.y > FALL_SPEED) ? FALL_SPEED : newVelocity.y, newVelocity.z);
+                    object.setVelocity(newVelocity);
 
-                if(forwardPressed || leftPressed || rightPressed || backPressed) {
-                    if(acceleration < MAX_SPEED)
-                        acceleration += 0.1;
-                } else if (acceleration > 0.2) {
+                    if(forwardPressed || leftPressed || rightPressed || backPressed) {
+                        if(acceleration < speed.getValueFloat())
+                            acceleration += 0.1;
+                    } else if (acceleration > 0.2) {
                         acceleration -= 0.2;
+                    }
                 }
-            }
 
-            if (toggle == 0 || newVelocity.y <= -0.04) {
-                toggle = 40;
+                if (toggle == 0 || newVelocity.y <= -0.04) {
+                    toggle = 40;
+                }
+                toggle--;
             }
-            toggle--;
         }
         super.onTick();
     }
--- src/main/java/net/snomn/snowhack/hack/movement/Flight.java
+++ src/main/java/net/snomn/snowhack/hack/movement/Flight.java
@@ -8,7 +8,7 @@ import org.lwjgl.glfw.GLFW;
 
 public class Flight extends Hack {
 
-    public NumberSetting speed = new NumberSetting("Speed", 0, 10, 1, 0.1);
+    public NumberSetting speed = new NumberSetting("Speed", 0, 10, 0.1, 0.1);
     public BooleanSetting testBool = new BooleanSetting("Check", true);
     public ModeSetting testMode = new ModeSetting("Mode", "Test", "Test", "Test2", "Test3");
 
